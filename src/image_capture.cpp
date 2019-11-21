c271c25a5f0fe48b6f7c0079e0612b55cee4e9a9
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-11-21 12:06:04 -0900

    image_capture

--- src/image_capture.cpp
+++ src/image_capture.cpp
@@ -13,38 +13,35 @@
 
 //This function opens a new window with the computer's default camera settings and allows the user to save the current frame as a jpg.
 //It returns true if an image was saved or false if it failed to open the camera or was closed before a picture was taken.
-bool image_capture()
+
+void image_capture()
+
 {
 	char key;
 	cv::VideoCapture cap(0);
 
-	//Check for any failure in opening the camera
 	if (!cap.isOpened())
 	{
 		std::cout << "Error: Cannot open camera.";
-		return false;
+		return;
 	}
 
-	std::cout << "Press 'c' to capture an image.  Press 'q' to quit." << std::endl;
+	std::string windowName = "Press any key to take a picture.";
 
-	//Open a new window displaying the current camera feed
 	cv::Mat frame;
-	cv::namedWindow("Capture", cv::WINDOW_AUTOSIZE);
-
-	//Upon user input, either saves the current frame as "image.jpg" to the root directory or exits the function
-	bool captured = false;
+	cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
 	while (true)
 	{
 		cap >> frame;
-		cv::imshow("Capture", frame);
+		cv::imshow(windowName, frame);
 
 		key = cv::waitKey(30);
 
-		if (key == 'c')
+		if (key > 0)
 		{
 			cv::imwrite("image.jpg", frame);
-			std::cout << "Image captured." << std::endl;
-			captured = true;
+
+			break;
 		}
 		if (key == 'q')
 		{
@@ -52,5 +49,8 @@ bool image_capture()
 		}
 	}
 
-	return captured;
+	cv::destroyWindow(windowName);
+
+	return;
+
 }
