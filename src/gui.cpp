2b4644fd95af158b9044c9b3ff010fb3427177e4
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-11-21 11:55:49 -0900

    added gui.cpp and changed image_capture.cpp to integrate with gui

--- src/gui.cpp
+++ src/gui.cpp
@@ -6,50 +6,61 @@
  * Outputs data for facehasher into a GUI display
  */
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-
 #include "facehasher.hpp"
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::vector;
-using std::string;
-using std::noskipws;
-using std::getline;
-using std::istringstream;
-using std::find; 
-
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Button.H>
+#include <FL/FL_JPEG_Image.H>
+#include <FL/Fl_Shared_Image.H>
 
 #pragma comment(lib, "fltk.lib")
+#pragma comment(lib, "fltk_images.lib")
+#pragma comment(lib, "libpng16.lib")
+#pragma comment(lib, "jpeg.lib")
 #pragma comment(lib, "comctl32.lib")
+
 
 void OnCloseCallback(Fl_Widget* w, void* data) {
 	Fl_Window* window = (Fl_Window*)data;
 	if (window) window->hide();
 }
 
-int main(int argc, char** argv) {
-	Fl_Window* window = new Fl_Window(340, 240);
+void CameraCallback(Fl_Widget* w, void* data) {
+	image_capture();
+	Fl_Box* imgbox = (Fl_Box*)data;
+	imgbox->redraw();
 
-	Fl_Box* box = new Fl_Box(20, 40, 300, 100, "Hello, World!");
-	box->box(FL_UP_BOX);
-	box->labelfont(FL_BOLD + FL_ITALIC);
-	box->labelsize(36);
-	box->labeltype(FL_SHADOW_LABEL);
+	Fl_JPEG_Image* image = new Fl_JPEG_Image("image.jpg");
+	imgbox->image(image);
+}
+
+
+int main(int argc, char** argv) {
+	fl_register_images();
+	Fl_Window* window = new Fl_Window(1240, 640, "Facehasher");
 
 	const int ControlHeight = 25;
 
-	Fl_Button* closeBtn = new Fl_Button(20, 160, 300, ControlHeight, "Close Window");
+	Fl_JPEG_Image* image = new Fl_JPEG_Image("image.jpg");
+	Fl_Box* imgbox = new Fl_Box(20, 10, 700, 500);
+	imgbox->image(image);
+
+
+	Fl_Box* box = new Fl_Box(720, 40, 370, 70, "Press any key to take a Picture.");
+	box->box(FL_UP_BOX);
+	box->labelfont(FL_BOLD + FL_ITALIC);
+	box->labelsize(24);
+	box->labeltype(FL_SHADOW_LABEL);
+
+
+	Fl_Button* camBtn = new Fl_Button(720, 120, 250, ControlHeight, "Take Picture");
+	camBtn->callback(CameraCallback, (void*)imgbox);
+
+
+	Fl_Button* closeBtn = new Fl_Button(720, 200, 250, ControlHeight, "Close Window");
 	closeBtn->callback(OnCloseCallback, (void*)window);
+
 
 	window->end();
 	window->show(argc, argv);
